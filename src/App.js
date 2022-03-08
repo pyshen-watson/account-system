9312eae2ecd1e77755fa0eaf23a812120305a0fe
pyshen <pyshen.watson@gmail.com>
2022-03-08 20:23:13 +0800

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Account from './components/Account'
 function App() {
   return (
     <div className='container'>
-      <Account name={"Cash"} />
-      <Account name={"Card"} />
-      <Account name={"Coin"} />
-      <Account name={"Post Account"} />
-      <Account name={"Richart"} />
+      <Account name={"現金 Cash"} />
+      <Account name={"悠遊卡 Card"} />
+      <Account name={"大祕寶 Coin"} />
+      <Account name={"郵局帳戶 Post Account"} />
+      <Account name={"台新帳戶 Richart"} />
     </div>
     )
 }
