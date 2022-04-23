b740bbf51a183ebd755e9b460202145a09739d58
pyshen <pyshen.watson@gmail.com>
2022-04-24 06:50:49 +0800

    redesign

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Account from './components/Account'
 
 function App() {
   return (
-    <div className='container'>
+    <main>
       <Account name={"現金 Cash"} />
       <Account name={"悠遊卡 Card"} />
       <Account name={"大祕寶 Coin"} />
-      <Account name={"郵局帳戶 Post Account"} />
+      <Account name={"郵局帳戶 Post"} link={"https://ipost.post.gov.tw/pst/home.html"} />
       <Account name={"台新帳戶 Richart"} />
-    </div>
+    </main>
     )
 }
 
--- src/components/Account.js
+++ src/components/Account.js
@@ -1,52 +1,75 @@
 import React, { useState } from 'react'
 import './Account.css'
 
-const inputHandler = (setter) => {
-    return (e) => {
-        let value = parseInt(e.target.value)
-        setter(value)
-    }
+
+const diffColor = (realValue, recordValue) => {
+    if(realValue > recordValue)
+        return "red"
+    else if (realValue < recordValue)
+        return "blue"
+    else
+        return "green"
 }
 
-export default function Account({name}) {
+
+export default function Account({name, link=""}) {
 
 
-    const [realValue, setRealValue] = useState(0)
-    const [recordValue, setRecordValue] = useState(0)
+    const [realValue, setRealValue] = useState('0')
+    const [recordValue, setRecordValue] = useState('0')
 
-    var diffClass = "green"
+    var diffClass = diffColor(realValue, recordValue)
 
-    if (realValue > recordValue)
-        diffClass = "red"
-    else if (realValue < recordValue)
-        diffClass = "blue"
+    const handleInput = (setter) => {
+        return (e) => {
+            var intValue = parseInt(e.target.value)
+            setter(intValue.toString())
+        }
+    }
+
+    const resetInput = () => {
+        setRecordValue('0')
+        setRealValue('0')
+    }
+
+    const diff = parseInt(recordValue)-parseInt(realValue)
+
+
+    const legend = link.length === 0 ?
+        <p>{name}</p>:
+        <a href={link} target="_blank" rel="noreferrer">{name}</a>
+
+
+
 
 
     return (
-        <fieldset className="account">
-            <legend>{name}</legend>
 
-            <div className="row">
-                <span>帳面餘額：</span>
-                <input 
-                    type="text" 
-                    onInput={inputHandler(setRealValue)}
+        <div className="container">
+            <h3>{legend}</h3>
+
+            <div>
+                <span>紀錄餘額：</span>
+                <input
+                    type="text"
+                    value={realValue}
+                    onChange={handleInput(setRealValue)}
                 />
             </div>
-            
-            <div className="row">
+
+            <div>
                 <span>實際餘額：</span>
-                <input 
-                    type="text" 
-                    onInput={inputHandler(setRecordValue)}
+                <input
+                    type="text"
+                    value={recordValue}
+                    onChange={handleInput(setRecordValue)}
                 />
             </div>
-            
-            <div className="row">
-                <span>差額：</span>
-                <span className={diffClass}>{recordValue- realValue}</span>
-            </div>
 
-        </fieldset>
+            <div className='control'>
+                <span className={diffClass}>差額：{diff}</span>
+                <button onClick={resetInput}>歸零</button>
+            </div>
+        </div>
     )
 }
